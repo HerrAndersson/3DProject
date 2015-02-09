37aa1793c695935b8ed9137d478b556fe1b1c340
Mattias Lunderot <matriz@live.se>
2015-02-09 20:29:31 +0100

    Reverted shaders

--- assets/shaders/vsTerrain.hlsl
+++ assets/shaders/vsTerrain.hlsl
@@ -9,94 +9,29 @@ cbuffer MatrixBuffer
 
 struct VS_IN
 {
-	float4 position : POSITION;
-	float2 tex : TEXCOORD0;
-	float3 normal : NORMAL;
+	float3 Pos : POSITION;
+	float3 Color : COLOR;
 };
 
 struct VS_OUT
 {
-	float4 position : SV_POSITION;
-	float2 tex : TEXCOORD0;
-	float3 normal : NORMAL;
+	float4 Pos : SV_POSITION;
+	float3 Color : COLOR;
 };
 
 VS_OUT main(VS_IN input)
 {
 	VS_OUT output = (VS_OUT)0;
 
-	// Change the position vector to be 4 units for proper matrix calculations.
-	input.position.w = 1.0f;
+	output.Pos = float4(input.Pos, 1);
 
-	// Calculate the position of the vertex against the world, view, and projection matrices.
-	output.position = mul(input.position, wvpMatrix);
-	//output.position = mul(input.position, worldMatrix);
-	//output.position = mul(output.position, viewMatrix);
-	//output.position = mul(output.position, projectionMatrix);
+	//output.Pos = mul(input.Pos, wvpMatrix);
 
-	// Store the texture coordinates for the pixel shader.
-	output.tex = input.tex;
+	//output.Pos = mul(input.Pos, worldMatrix);
+	output.Pos = mul(output.Pos, viewMatrix);
+	output.Pos = mul(output.Pos, projectionMatrix);
 
-	// Calculate the normal vector against the world matrix only.
-	output.normal = mul(input.normal, (float3x3)worldMatrix);
-
-	// Normalize the normal vector.
-	output.normal = normalize(output.normal);
+	output.Color = input.Color;
 
 	return output;
 }
-
-//struct VS_IN
-//{
-//	float3 Pos : POSITION;
-//	float3 Color : COLOR;
-//};
-//
-//struct VS_OUT
-//{
-//	float4 Pos : SV_POSITION;
-//	float3 Color : COLOR;
-//};
-
-//VS_OUT main(VS_IN input)
-//{
-//	VS_OUT output = (VS_OUT)0;
-//
-//	output.Pos = float4(input.Pos, 1);
-//
-//	//output.Pos = mul(input.Pos, wvpMatrix);
-//
-//	//output.Pos = mul(input.Pos, worldMatrix);
-//	output.Pos = mul(output.Pos, viewMatrix);
-//	output.Pos = mul(output.Pos, projectionMatrix);
-//
-//	output.Color = input.Color;
-//
-//	return output;
-//}
-
-
-
-//PS
-//Texture2D shaderTexture;
-//SamplerState SampleType;
-//
-//cbuffer LightBuffer
-//{
-//	float4 ambientColor;
-//	float4 diffuseColor;
-//	float3 lightDirection;
-//	float padding;
-//};
-
-//struct VS_OUT
-//{
-//	float4 position : SV_POSITION;
-//	float2 tex : TEXCOORD0;
-//	float3 normal : NORMAL;
-//};
-//
-//float4 main(VS_OUT input) : SV_Target
-//{	
-//	return input.position;
-//}
