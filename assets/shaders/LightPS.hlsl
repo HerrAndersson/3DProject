d21ee8531a61bae1ee045f1f9665ae7e8584b43c
HerrAndersson <anderssonjonas94@gmail.com>
2015-03-29 23:39:35 +0200

    Simplified shadowMap sampling

--- assets/shaders/LightPS.hlsl
+++ assets/shaders/LightPS.hlsl
@@ -32,29 +32,30 @@ float4 main(VS_OUT input) : SV_TARGET
 
 	float depth = shadowPos.z / shadowPos.w;
 
-	// 2x2 percentage closest filter. POINT SAMPLING!
-	float dx = 1.0f / size; // size of one texture sample in the shadow map (width==height)
-	float s0 = (shadowTexture.Sample(SampleTypePoint, smTex).r + 0.0005f < depth) ? 0.0f : 1.0f;
-	float s1 = (shadowTexture.Sample(SampleTypePoint, smTex + float2(dx, 0.0f)).r + 0.0005f < depth) ? 0.0f : 1.0f;
-	float s2 = (shadowTexture.Sample(SampleTypePoint, smTex + float2(0.0f, dx)).r + 0.0005f < depth) ? 0.0f : 1.0f;
-	float s3 = (shadowTexture.Sample(SampleTypePoint, smTex + float2(dx, dx)).r + 0.0005f < depth) ? 0.0f : 1.0f;
+	//// 2x2 percentage closest filter. POINT SAMPLING!
+	//float dx = 1.0f / size; // size of one texture sample in the shadow map (width==height)
+	//float s0 = (shadowTexture.Sample(SampleTypePoint, smTex).r + 0.0005f < depth) ? 0.0f : 1.0f;
+	//float s1 = (shadowTexture.Sample(SampleTypePoint, smTex + float2(dx, 0.0f)).r + 0.0005f < depth) ? 0.0f : 1.0f;
+	//float s2 = (shadowTexture.Sample(SampleTypePoint, smTex + float2(0.0f, dx)).r + 0.0005f < depth) ? 0.0f : 1.0f;
+	//float s3 = (shadowTexture.Sample(SampleTypePoint, smTex + float2(dx, dx)).r + 0.0005f < depth) ? 0.0f : 1.0f;
 
-	// Transform shadow map UV coord to texel space
-	float2 texelPos = smTex * size;
+	//// Transform shadow map UV coord to texel space
+	//float2 texelPos = smTex * size;
 
-	// Determine the lerp amount, if  = (340.3, 200.1) then  = (0.3,0.1)
-	float2 lerps = frac(texelPos);
-	float shadowCoeff = lerp(lerp(s0, s1, lerps.x), lerp(s2, s3, lerps.x), lerps.y);
+	//// Determine the lerp amount, if  = (340.3, 200.1) then  = (0.3,0.1)
+	//float2 lerps = frac(texelPos);
+	//float shadowCoeff = lerp(lerp(s0, s1, lerps.x), lerp(s2, s3, lerps.x), lerps.y);
 
 	//Get local illumination from the "sun" on the whole scene
 	float3 lightDir = -lightDirection;										 // Invert the light direction for calculations.
 	float lightIntensity = saturate(dot(normal.xyz, lightDir)) + 0.35;		 // Calculate the amount of light on this pixel.
 	float4 outputColor = saturate(col * lightIntensity);					 // Determine the final amount of diffuse color based on the color of the pixel combined with the light intensity.
 
-	if (shadowCoeff >= depth)
-	{
-		return outputColor * shadowCoeff;
-	}
-	
-	return outputColor / 1.3;
+
+
+
+	float depthSample = (shadowTexture.Sample(SampleTypePoint, smTex).r + 0.0005f < depth) ? 0.0f : 1.0f;
+
+	return outputColor * depthSample;
+
 }
